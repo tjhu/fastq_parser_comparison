4c9378ed5be8bdebb53d081b146509d17c21f485
Tianjiao Huang <git@tjhu.dev>
2021-10-20 19:17:47 -0400

    readfq support gz

--- main.cpp
+++ main.cpp
@@ -12,23 +12,22 @@
 #include "readfq/kseq.h"
 #include "fxtract/kseq.hpp"
 
-ABSL_FLAG(std::string, input_file, "SRR072006.fastq", "input file");
+ABSL_FLAG(std::string, input_file, "SRR072006.fastq.gz", "input file");
 
 typedef void (*benchmark_fn_t)(std::string_view);
 
-KSEQ_INIT(int, read)
+KSEQ_INIT(gzFile, gzread)
 void readfq(std::string_view input_file) {
-  int fd = open(input_file.data(), O_RDONLY);
-  PCHECK(fd >= 0) << "Failed to open file: " << input_file;
+  gzFile file = gzopen(input_file.data(), "r");
 
-  kseq_t* seq = kseq_init(fd);
+  kseq_t* seq = kseq_init(file);
   while (kseq_read(seq) >= 0) {
     // noop
     // puts(seq->seq.s);
   }
   kseq_destroy(seq);
 
-  close(fd);
+  gzclose(file);
 }
 
 void fxtract1(std::string_view input_file) {
@@ -40,6 +39,7 @@ void fxtract1(std::string_view input_file) {
     // noop
     // std::cout << seq.seq;
   }
+  gzclose(file);
 }
 
 void read(std::string_view input_file) {
@@ -53,6 +53,18 @@ void read(std::string_view input_file) {
   }
 
   close(fd);
+}
+
+void gzread(std::string_view input_file) {
+  gzFile file = gzopen(input_file.data(), "r");
+
+  constexpr size_t BUFF_SIZE = 4096;
+  char buff[BUFF_SIZE];
+  while (gzread(file, &buff, BUFF_SIZE) > 0) {
+    // noop
+  }
+
+  gzclose(file);
 }
 
 void benchmark(std::string_view name, benchmark_fn_t fn,
@@ -74,13 +86,17 @@ int main(int argc, char** argv) {
   // Always log.
   FLAGS_logtostderr = 1;
 
-  // Parse command line
+  // Parse command line.
   absl::ParseCommandLine(argc, argv);
 
+  // Validate arguments.
   std::string input_file = absl::GetFlag(FLAGS_input_file);
   CHECK_NE(input_file, "") << "Must specify a input file";
 
+  // Run benchmarks.
+  benchmark("warmup", read, input_file);
   benchmark("read", read, input_file);
+  benchmark("gzread", gzread, input_file);
   benchmark("readfq", readfq, input_file);
   benchmark("fxtract", fxtract1, input_file);
 
